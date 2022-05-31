41c2b472cac6b7c8c54cac4aff72888203b83da4
krishnendu basak <krisauz17@yahoo.com>
2022-05-31 21:39:44 +1000

    programming-univbasics-4-basic-hashes-lab is done

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,21 +1,27 @@
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
   {key => value}
+  {:name => "Grace Hopper", 1=>2}
+
 end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
   hash[key]
+  read_from_hash[:name] = "Steve"
+  read_from_hash[:name] = "Tzvi"
+  read_from_hash[:age] = 31
 end
 
 def update_counting_hash(hash, key)
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
-  if hash[key]
-    hash[key]+=1
+  update_counting_hash{:total=>5, :age}
+  if update_counting_hash[age]
+    update_counting_hash[age]+=1
   else
-    hash[key] = 1
+    update_counting_hash[age] = 1
   end
-  hash
+    update_counting_hash
 end
